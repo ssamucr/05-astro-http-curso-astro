e5eca28dc94da1d444605bedd34602b00c72b36e
ssamucr <ssamucr@gmail.com>
2026-03-31 20:10:13 -0500

    Added node integration for server side rendering. Added a server side rendered datetime at home page. Created a GET Endpoint that returns a post if a query parameter is defined, if not, return all the posts.

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,21 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
 
+import node from '@astrojs/node';
+
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+  site: 'https://example.com',
+  integrations: [mdx(), sitemap()],
+  output: 'static',
+  /*
+  si pongo el output en server todo se renderiza del lado del servidor por defecto y para que algo se genere estatico necesito
+  export const prerender = true;
+
+  si pongo el output en static todo se renderiza del lado del cliente por defecto y para que algo se genere del lado del servidor necesito
+  export const prerender = false;
+  */
+  adapter: node({
+    mode: 'standalone',
+  }),
 });
--- src/pages/api/posts/index.ts
+++ src/pages/api/posts/index.ts
@@ -1,12 +1,36 @@
 import type { APIRoute } from "astro";
-import { getCollection } from "astro:content";
+import { getCollection, getEntry } from "astro:content";
+
+export const prerender = false;
 
 export const GET: APIRoute = async ({ params, request }) => {
 
+  const url = new URL(request.url);
+  const slug = url.searchParams.get("slug");
+
+  if (slug) {
+    const post = await getEntry("blog", slug);
+    
+    if (post) {
+      return new Response(JSON.stringify(post), {
+        status: 200,
+        headers: { "Content-Type": "application/json" },
+      });
+    }
+
+    return new Response(JSON.stringify({ error: `Post not found: ${slug}` }), {
+      status: 404,
+      headers: { "Content-Type": "application/json" },
+    });
+
+  }
+  else {
     const posts = await getCollection("blog");
 
-  return new Response(JSON.stringify(posts), {
-    status: 200,
-    headers: { "Content-Type": "application/json" },
-  })
+    return new Response(JSON.stringify(posts), {
+      status: 200,
+      headers: { "Content-Type": "application/json" },
+    })
+  }
+
 };
