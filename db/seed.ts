168a0743b1a436858b2f8ad6e1ae08335887a95b
ssamucr <ssamucr@gmail.com>
2026-04-01 20:57:02 -0500

    Created and seeded a tabla with astro db

--- db/seed.ts
+++ db/seed.ts
@@ -1,6 +1,16 @@
-import { db } from 'astro:db';
+import { Clients, db } from 'astro:db';
 
 // https://astro.build/db/seed
 export default async function seed() {
 	// TODO
+	console.log("Seeding database...");
+
+	await db.insert(Clients).values([
+		{ name: "Kasim", age: 30, isActive: true },
+		{ name: "Mina", age: 25, isActive: false },
+		{ name: "Liam", age: 28, isActive: true },
+		{ name: "Sophia", age: 22, isActive: true },
+		{ name: "Ethan", age: 35, isActive: false },
+	]);
+
 }
